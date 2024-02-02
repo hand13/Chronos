59642fccd2caf81cfbf9adaa667dc579ad4bc38e
hand13 <1101738345@qq.com>
2024-02-02 22:27:58 +0800

    poco

--- src/Log.cpp
+++ src/Log.cpp
@@ -2,9 +2,13 @@
 #include <iostream>
 #include "Log.h"
 namespace Chronos {
-    void Log(const std::wstring& msg) {
+    void Log(const std::wstring& msg,unsigned int level) {
         std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> conversion;
         std::string out = conversion.to_bytes(msg);
-        std::cout<<out<<std::endl;
+        if(level == 0){
+            std::cout<<out<<std::endl;
+        }else {
+            std::cerr<<out<<std::endl;
+        }
     }
 }
--- src/Log.h
+++ src/Log.h
@@ -5,5 +5,5 @@ namespace Chronos {
     inline void Log(const std::string& msg) {
         std::cout<<msg<<std::endl;
     }
-    void Log(const std::wstring& msg);
+    void Log(const std::wstring& msg,unsigned int level = 0);
 }
